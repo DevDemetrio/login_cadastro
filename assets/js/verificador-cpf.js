574e2bf2c1b54c6e116de5d933580b18913af3bd
Devdemetrio <devdemetrio@gmail.com>
2023-03-08 20:41:44 -0400

    foi implementado a altenticação do CPF

--- assets/js/verificador-cpf.js
+++ assets/js/verificador-cpf.js
@@ -1,5 +1,7 @@
 var botao = document.querySelector("#botao");
 var somma = 0;
+var numerosPrimeiroDigito = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+var numerosSegundoDigito = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
 botao.addEventListener("click", function (event) {
   event.preventDefault();
 
@@ -8,9 +10,35 @@ botao.addEventListener("click", function (event) {
   var array = cpf.value.split("");
 
   for (var i = 0; i < 9; i++) {
-    somma = array[i];
+    somma += parseInt(array[i]) * numerosPrimeiroDigito[i];
   }
+
+  if (encontraDigito(somma % 11) == parseInt(array[9])) {
+    console.log("Meus filhos são vitoriosos");
+  } else {
+    console.log("CPF errado");
+  }
+
+  somma = 0;
+  for (var j = 0; j < 10; j++) {
+    console.log(array[j], numerosSegundoDigito[j]);
+    somma += parseInt(array[j]) * numerosSegundoDigito[j];
+  }
+
   console.log(somma);
+  if (encontraDigito(somma % 11) == parseInt(array[10])) {
+    console.log("Meus filhos são vitoriosos2");
+  } else {
+    console.log("CPF errado 2");
+  }
 });
+
+function encontraDigito(numero) {
+  if (numero < 2) {
+    return 0;
+  } else {
+    return 11 - numero;
+  }
+}
 
 /*https://www.macoratti.net/alg_cpf.htm */
